7af279c80a11f1455a9412197d6d5e560d78f611
dofxo <kargat504@gmail.com>
2023-07-22 01:28:09 +0330

    feat(Card): add Card component with it's styles

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+
+const Card = () => {
+	return (
+		<div className="w-56 h-56 rounded-md bg-purple-300 shadow-purple-600 shadow-inner">
+
+		</div>
+	)
+}
+
+export default Card
--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,8 +1,11 @@
+// child components
+import Card from "./Card"
+
 const GameContainer = () => {
-  return (
-    <div className="game-container flex flex-wrap gap-6 mt-10" >
-    </div>
-  )
+	return (
+		<div className="game-container flex flex-wrap gap-6 mt-10 justify-between" >
+		</div>
+	)
 }
 
 export default GameContainer
