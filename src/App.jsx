326bd0ef7072e56d47b357fb960531ca42d59f9a
dofxo <kargat504@gmail.com>
2023-07-22 01:15:13 +0330

    feat(gameContainer): add game container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import GameContainer from "./components/GameContainer"
 
 const App = () => {
 	return (
 		<>
 		<div className="container">
 		<h1 className="text-center text-4xl font-bold">Memory Game</h1>
+    <GameContainer />
 		</div>
 		</>
 	)
---
+++ src/components/GameContainer.jsx
@@ -0,0 +1,8 @@
+const GameContainer = () => {
+  return (
+    <div className="game-container flex flex-wrap gap-6">
+    </div>
+  )
+}
+
+export default GameContainer
