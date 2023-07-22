34cb3196317e74d80cddf82962daa70bc98d5856
dofxo <kargat504@gmail.com>
2023-07-22 13:46:54 +0330

    refactor(Cards list): stores the card inside a state

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -6,15 +6,18 @@ import cardsList from "../data/cardsList";
 
 // elpers
 import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
 const GameContainer = () => {
   const shuffledList = [...cardsList, ...cardsList].sort(
     () => Math.random() - 0.5
   );
 
+  const [cards, setCards] = useState(shuffledList);
+
   return (
     <div className="game-container flex flex-wrap gap-6 mt-5 justify-between">
-      {shuffledList.map((card) => (
+      {cards.map((card) => (
         <Card key={uuidv4()} cardDetails={card} />
       ))}
     </div>
