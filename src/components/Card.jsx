b1dbe5ab4f3e3681150cacfb4eb85a5852faac06
dofxo <kargat504@gmail.com>
2023-07-22 12:01:43 +0330

    feat(Card): add Card styles and add a functionality to flip the cards on click

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,33 @@
+// elpers
+import { useState } from "react";
+import classNames from "classnames";
 
-const Card = () => {
-	return (
-		<div className="w-56 h-56 rounded-md bg-purple-300 shadow-purple-600 shadow-inner">
+// styles
+import "../styles/components/Card.scss";
 
-		</div>
-	)
-}
+const Card = ({ cardDetails }) => {
+  // satate to track the clicked status on cards
+  const [clicked, setClicked] = useState(false);
 
-export default Card
+  // class name for the card using "ClassNames"
+  const clickedClassName = classNames({
+    ["clicked-card"]: clicked,
+    card: true,
+  });
+  return (
+    <div
+      // change the card classList onClick to flip the card
+      onClick={() => {
+        setClicked((prev) => (prev = true));
+      }}
+      className={clickedClassName}
+    >
+      <div className="card-front">
+        <img src="../../assets/images/card-bg.jpg" alt="" />
+      </div>
+      <div className="card-back">{cardDetails}</div>
+    </div>
+  );
+};
+
+export default Card;
