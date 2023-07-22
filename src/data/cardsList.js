346cb26f01ffabad64ff05348edecb31db42c103
dofxo <kargat504@gmail.com>
2023-07-22 12:42:32 +0330

    refactor(cardListData): Change the card array

--- src/data/cardsList.js
+++ src/data/cardsList.js
@@ -1,3 +1,27 @@
-const cardsList = [1, 2, 3, 4, 5, 6];
+const cardsList = [
+	{
+		info: 1,
+		id: 1,
+	},
+	{
+		info: 2,
+		id: 2,
+	},
+	{
+		info: 3,
+		id: 3,
+	},
+	{
+		info: 4,
+		id: 4,
+	},
+	{
+		info: 5,
+		id: 5,
+	}, {
+		info: 6,
+		id: 6,
+	},
+];
 
 export default cardsList;
