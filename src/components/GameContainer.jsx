e395b77972b88305b2422d6125a199064d8b1bed
dofxo <kargat504@gmail.com>
2023-07-22 12:01:01 +0330

    refactor(GameContainer): used a shuffle list of the cardList to render the cards

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,11 +1,24 @@
 // child components
-import Card from "./Card"
+import Card from "./Card";
+
+// cards data
+import cardsList from "../data/cardsList";
+
+// elpers
+import { v4 as uuidv4 } from "uuid";
 
 const GameContainer = () => {
-	return (
-		<div className="game-container flex flex-wrap gap-6 mt-10 justify-between" >
-		</div>
-	)
-}
+  const shuffledList = [...cardsList, ...cardsList].sort(
+    () => Math.random() - 0.5
+  );
 
-export default GameContainer
+  return (
+    <div className="game-container flex flex-wrap gap-6 mt-10 justify-between">
+      {shuffledList.map((card) => (
+        <Card key={uuidv4()} cardDetails={card} />
+      ))}
+    </div>
+  );
+};
+
+export default GameContainer;
