8c8f4f9dc3dbdaa429bc9d0a4921f42019139445
dofxo <kargat504@gmail.com>
2023-07-22 12:29:12 +0330

    feat(ResetGameBtn): add the button to restart the game (W/O functionality))

---
+++ src/components/ResetGameButton.jsx
@@ -0,0 +1,11 @@
+
+const ResetGameButton = () => {
+  return (
+    <div className="flex justify-center mt-5">
+          <button className="text-white p-2 bg-purple-500 border border-transparent rounded hover:bg-transparent hover:border-purple-500 hover:border transition ">Restart the Game</button>
+    </div>
+
+  )
+}
+
+export default ResetGameButton
