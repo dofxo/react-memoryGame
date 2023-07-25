c63ce8e283ae983e450135818b41da2a4f1e9886
dofxo <kargat504@gmail.com>
2023-07-25 19:00:53 +0330

    refactor(cardList): add matched key to each card

--- src/data/cardsList.js
+++ src/data/cardsList.js
@@ -1,21 +1,27 @@
 const cardsList = [
   {
     info: 1,
+    matched: false,
   },
   {
     info: 2,
+    matched: false,
   },
   {
     info: 3,
+    matched: false,
   },
   {
     info: 4,
+    matched: false,
   },
   {
     info: 5,
+    matched: false,
   },
   {
     info: 6,
+    matched: false,
   },
 ];
 
