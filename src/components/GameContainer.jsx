0455efb67beb624203824f965684958a73b7e133
dofxo <kargat504@gmail.com>
2023-07-25 17:09:58 +0330

    refactor(GameContainer): remove the nex Levels of developement

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -7,55 +7,19 @@ import cardsList from "../data/cardsList";
 
 // elpers
 import { v4 as uuidv4 } from "uuid";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const GameContainer = () => {
   const [cards, setCards] = useState([]);
   const [turns, setTurn] = useState(0);
-
-  const [firstChoice, setFirstChoice] = useState(null);
-  const [secondChoice, setSecondChoice] = useState(null);
 
   const gameStartHanlde = () => {
     // shuffle list
     const shuffledList = [...cardsList, ...cardsList].sort(
       () => Math.random() - 0.5
     );
-
     setCards(shuffledList);
     setTurn(0);
-  };
-
-  // compairing the choices
-  useEffect(() => {
-    if (firstChoice && secondChoice) {
-      if (firstChoice.id === secondChoice.id) {
-        setCards((prev) => {
-          return prev.map((card) => {
-            if (card.id === firstChoice.id) {
-              return { ...card, matched: true };
-            } else {
-              return card;
-            }
-          });
-        });
-        resetTurn();
-      } else {
-        resetTurn();
-      }
-    }
-  }, [firstChoice, secondChoice]);
-
-  // handle card click
-  const handleClickCard = (card) => {
-    !firstChoice ? setFirstChoice(card) : setSecondChoice(card);
-  };
-
-  // restarting turns
-  const resetTurn = () => {
-    setFirstChoice(null);
-    setSecondChoice(null);
-    setTurn((prev) => (prev += 1));
   };
 
   return (
@@ -63,11 +27,7 @@ const GameContainer = () => {
       <StartGame gameStartHanlde={gameStartHanlde} turns={turns} />
       <div className="game-container flex flex-wrap gap-6 mt-5 justify-between">
         {cards.map((card) => (
-          <Card
-            key={uuidv4()}
-            cardDetails={card}
-            handleClickCard={handleClickCard}
-          />
+          <Card key={uuidv4()} cardDetails={card} />
         ))}
       </div>
     </>
