f5ebe0b7fdab6bb48523d533bdbca9a9a7253143
dofxo <kargat504@gmail.com>
2023-07-25 17:06:42 +0330

    refactor(cardList): update card list and removed the matched key

--- src/data/cardsList.js
+++ src/data/cardsList.js
@@ -2,32 +2,26 @@ const cardsList = [
   {
     info: 1,
     id: 1,
-    matched: false,
   },
   {
     info: 2,
     id: 2,
-    matched: false,
   },
   {
     info: 3,
     id: 3,
-    matched: false,
   },
   {
     info: 4,
     id: 4,
-    matched: false,
   },
   {
     info: 5,
     id: 5,
-    matched: false,
   },
   {
     info: 6,
     id: 6,
-    matched: false,
   },
 ];
 
