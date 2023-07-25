8533088a787ddadc53981a384527a591d1689bc7
dofxo <kargat504@gmail.com>
2023-07-25 20:24:37 +0330

    feat(Card): whenver the card clickes, it would disable the card for 800ms ( to prevent the clicking the card twice)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,14 @@
 // elpers
 import classNames from "classnames";
+import { useState } from "react";
 
 // styles
 import "../styles/components/Card.scss";
 
 const Card = ({ cardDetails, handleCardClick, flipped, disabled }) => {
+  // state to track the click on the card. if it has been clicked once, it could not be clicked twice.
+  const [clicked, setClickedStatus] = useState(false);
+
   // class name for the card using "ClassNames"
   const clickedClassName = classNames({
     ["clicked-card"]: flipped,
@@ -15,7 +19,11 @@ const Card = ({ cardDetails, handleCardClick, flipped, disabled }) => {
       <div
         // change the card classList onClick to flip the card if the disabled prop is false.
         onClick={() => {
-          !disabled ? handleCardClick(cardDetails) : "";
+          !disabled && !clicked ? handleCardClick(cardDetails) : "";
+          setClickedStatus(true);
+          setTimeout(() => {
+            setClickedStatus(false);
+          }, 800);
         }}
         className="card-front"
       >
