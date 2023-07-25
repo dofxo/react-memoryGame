8eb731247455ef111657d3bb8e75429e1c88a61b
dofxo <kargat504@gmail.com>
2023-07-25 17:45:53 +0330

    remove(cardList): removed the ID key in cardList

--- src/data/cardsList.js
+++ src/data/cardsList.js
@@ -1,27 +1,21 @@
 const cardsList = [
   {
     info: 1,
-    id: 1,
   },
   {
     info: 2,
-    id: 2,
   },
   {
     info: 3,
-    id: 3,
   },
   {
     info: 4,
-    id: 4,
   },
   {
     info: 5,
-    id: 5,
   },
   {
     info: 6,
-    id: 6,
   },
 ];
 
