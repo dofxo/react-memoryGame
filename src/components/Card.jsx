1f8ad2b4dc3b555a3f377dc87527db749f11f9d5
dofxo <kargat504@gmail.com>
2023-08-14 20:31:06 +0330

    refactor(deploy.yml): change assets path

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,34 +6,34 @@ import { useState } from "react";
 import "../styles/components/Card.scss";
 
 const Card = ({ cardDetails, handleCardClick, flipped, disabled }) => {
-  // state to track the click on the card. if it has been clicked once, it could not be clicked twice.
-  const [clicked, setClickedStatus] = useState(false);
+	// state to track the click on the card. if it has been clicked once, it could not be clicked twice.
+	const [clicked, setClickedStatus] = useState(false);
 
-  // class name for the card using "ClassNames"
-  const clickedClassName = classNames({
-    ["clicked-card"]: flipped,
-    card: true,
-  });
-  return (
-    <div className={clickedClassName}>
-      <div
-        // change the card classList onClick to flip the card if the disabled prop is false.
-        onClick={() => {
-          !disabled && !clicked ? handleCardClick(cardDetails) : "";
-          setClickedStatus(true);
-          setTimeout(() => {
-            setClickedStatus(false);
-          }, 800);
-        }}
-        className="card-front"
-      >
-        <img src="../../assets/images/card-game-bg.png" alt="" />
-      </div>
-      <div className="card-back">
-        <img src={cardDetails.info} alt="" />
-      </div>
-    </div>
-  );
+	// class name for the card using "ClassNames"
+	const clickedClassName = classNames({
+		["clicked-card"]: flipped,
+		card: true,
+	});
+	return (
+		<div className={clickedClassName}>
+			<div
+				// change the card classList onClick to flip the card if the disabled prop is false.
+				onClick={() => {
+					!disabled && !clicked ? handleCardClick(cardDetails) : "";
+					setClickedStatus(true);
+					setTimeout(() => {
+						setClickedStatus(false);
+					}, 800);
+				}}
+				className="card-front"
+			>
+				<img src="./assets/images/card-game-bg.png" alt="" />
+			</div>
+			<div className="card-back">
+				<img src={cardDetails.info} alt="" />
+			</div>
+		</div>
+	);
 };
 
 export default Card;
--- src/data/cardsList.js
+++ src/data/cardsList.js
@@ -1,28 +1,28 @@
 const cardsList = [
-  {
-    info: `/assets/images/card1.jpg`,
-    matched: false,
-  },
-  {
-    info: `/assets/images/card2.avif`,
-    matched: false,
-  },
-  {
-    info: `/assets/images/card3.webp`,
-    matched: false,
-  },
-  {
-    info: `/assets/images/card4.png`,
-    matched: false,
-  },
-  {
-    info: `/assets/images/card5.jpg`,
-    matched: false,
-  },
-  {
-    info: `/assets/images/card6.webp`,
-    matched: false,
-  },
+	{
+		info: `./assets/images/card1.jpg`,
+		matched: false,
+	},
+	{
+		info: `./assets/images/card2.avif`,
+		matched: false,
+	},
+	{
+		info: `./assets/images/card3.webp`,
+		matched: false,
+	},
+	{
+		info: `./assets/images/card4.png`,
+		matched: false,
+	},
+	{
+		info: `./assets/images/card5.jpg`,
+		matched: false,
+	},
+	{
+		info: `./assets/images/card6.webp`,
+		matched: false,
+	},
 ];
 
 export default cardsList;
